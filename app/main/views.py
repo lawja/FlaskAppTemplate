302ca22912fa6dd3c304e04312b4999e59bbddb4
Jake Lawrence <jake.lawrence24@gmail.com>
2017-11-22 16:21:14 -0500

    some modules functionality added

--- app/main/views.py
+++ app/main/views.py
@@ -2,6 +2,8 @@ from flask import (abort, jsonify, g, session, render_template, redirect,
                    request, url_for)
 from manage import app#, client
 from . import main
+# import self written modules from modules dir
+# from ..modules import ...
 
 @main.route('/')
 def index():
